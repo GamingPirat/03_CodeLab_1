2cac6ec45d8f2253b0ad05405f45269a3ee41f45
Michael <GamingPirat@gmail.com>
2024-07-17 09:12:29 +0200

    02 / Codechellange

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,10 +5,12 @@ public class App {
         Konto konto1 = new Konto();
         Konto konto2 = new Konto();
         Konto konto3 = new Konto();
-
+        Konto konto4 = new Konto();
+        
         output("Anfangs-Kontostand von konto1: " + konto1.getKontostand());
         output("Anfangs-Kontostand von konto2: " + konto2.getKontostand());
         output("Anfangs-Kontostand von konto3: " + konto3.getKontostand());
+        output("Anfangs-Kontostand von konto4: " + konto4.getKontostand());
 
         System.out.println("-----------------------------------------");
         
@@ -16,10 +18,11 @@ public class App {
         konto1.setKontostand(konto1.getKontostand() * 2);
         konto2.setKontostand(konto2.getKontostand() * 3);
         konto3.setKontostand(konto3.getKontostand() * 10);
-
+        konto4.setKontostand(konto4.getKontostand() * 8);
         output("Aktueller Kontostand von konto1: " + konto1.getKontostand());
         output("Aktueller Kontostand von konto2: " + konto2.getKontostand());
         output("Aktueller Kontostand von konto3: " + konto3.getKontostand());
+        output("Aktueller Kontostand von konto4: " + konto4.getKontostand());
     }
 
     private static void output(String outputStr) {
